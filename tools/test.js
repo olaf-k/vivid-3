2d7f9c66fdf276a4f11dda351c9db329e6230464
olaf-k <olaf-k@users.noreply.github.com>
2023-08-22 17:02:45 +0200

    Update test.js

--- tools/test.js
+++ tools/test.js
@@ -1,4 +1,5 @@
 const { exec } = require('node:child_process');
+const { log } = require('node:console');
 
 const NOCHANGE_PREFIX = 'no-change-for-';
 
@@ -9,11 +10,14 @@ exec('git branch --show-current', (err, branch) => {
     }
     branch = branch.trim();
 
-    exec('git diff origin/main... --name-status --diff-filter=d', (err, status) => {
+    // exec('git diff origin/main... --name-status --diff-filter=d', (err, status) => {
+    exec('git diff --name-status --diff-filter=d', (err, status) => {
         if (err) {
             console.error(`exec error while retrieving commit files list: ${err}`);
             process.exit(-1);
         }
+
+        console.log(status);
 
         const files = status.trim().split('\n').map(s => s.substring(s.lastIndexOf('\t') + 1));
         if (files.filter(name => name === NOCHANGE_PREFIX + branch || /^\.changeset\/[-\w]+\.md$/.test(name)).length === 0) {
