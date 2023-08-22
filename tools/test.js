d33ed1f8a27aab52c117a42afa37402a9eaca78c
olaf-k <olaf-k@users.noreply.github.com>
2023-08-22 18:02:44 +0200

    nbvnvb

--- tools/test.js
+++ tools/test.js
@@ -1,27 +1,16 @@
 const { exec } = require('node:child_process');
-const { log } = require('node:console');
 
-const NOCHANGE_PREFIX = 'no-change-for-';
+const NOCHANGEBRANCH = `.changeset/no-change-for-${process.env.BRANCH_NAME}`;
 
-exec('git branch --show-current', (err, branch) => {
+exec('git diff origin/main... --name-status --diff-filter=d', (err, status) => {
     if (err) {
-        console.error(`exec error while retrieving branch name: ${err}`);
+        console.error(`exec error while retrieving commit files list: ${err}`);
         process.exit(-1);
     }
-    branch = branch.trim();
 
-    console.log('branch=', branch);
-
-    exec('git diff origin/main... --name-status --diff-filter=d', (err, status) => {
-        if (err) {
-            console.error(`exec error while retrieving commit files list: ${err}`);
-            process.exit(-1);
-        }
-
-        const files = status.trim().split('\n').map(s => s.substring(s.lastIndexOf('\t') + 1));
-        if (files.filter(name => name === `.changeset/${NOCHANGE_PREFIX}${branch}` || /^\.changeset\/[-\w]+\.md$/.test(name)).length === 0) {
-            console.error(`⚠️ no changeset or bypass file found, please run npm whatever`);
-            process.exit(-1);
-        }
-    });
-}); 
+    const files = status.trim().split('\n').map(s => s.substring(s.lastIndexOf('\t') + 1));
+    if (files.filter(name => name === NOCHANGEBRANCH || /^\.changeset\/[-\w]+\.md$/.test(name)).length === 0) {
+        console.error(`⚠️ no changeset or bypass file found, please run npm whatever`);
+        process.exit(-1);
+    }
+});
