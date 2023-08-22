2e44fc6e470f1a4ab8702e75013dd162f519881c
olaf-k <olaf-k@users.noreply.github.com>
2023-08-22 17:42:19 +0200

    Update test.js

--- tools/test.js
+++ tools/test.js
@@ -10,7 +10,7 @@ exec('git branch --show-current', (err, branch) => {
     }
     branch = branch.trim();
 
-    console.log(branch);
+    console.log('branch=', branch);
 
     exec('git diff origin/main... --name-status --diff-filter=d', (err, status) => {
         if (err) {
@@ -19,12 +19,8 @@ exec('git branch --show-current', (err, branch) => {
         }
 
         const files = status.trim().split('\n').map(s => s.substring(s.lastIndexOf('\t') + 1));
-        if (files.filter(name => name === `.changeset/${NOCHANGE_PREFIX}${branch}`).length === 0) {
-            console.error(`⚠️ no bypass file found, please run npm whatever`);
-            process.exit(-1);
-        }
-        if (files.filter(name => /^\.changeset\/[-\w]+\.md$/.test(name)).length === 0) {
-            console.error(`⚠️ no changeset file found, please run npm whatever`);
+        if (files.filter(name => name === `.changeset/${NOCHANGE_PREFIX}${branch}` || /^\.changeset\/[-\w]+\.md$/.test(name)).length === 0) {
+            console.error(`⚠️ no changeset or bypass file found, please run npm whatever`);
             process.exit(-1);
         }
     });
