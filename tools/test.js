6574e0bcefc360fb865ea8a99dc420cee9ce6011
olaf-k <olaf-k@users.noreply.github.com>
2023-08-22 14:42:33 +0200

    go

---
+++ tools/test.js
@@ -0,0 +1,23 @@
+const { exec } = require('node:child_process');
+
+const NOCHANGE_PREFIX = 'no-change-for-';
+
+exec('git branch --show-current', (err, branch) => {
+    if (err) {
+        console.error(`exec error while retrieving branch name: ${err}`);
+        process.exit(-1);
+    }
+    branch = branch.trim();
+
+    exec('git diff origin/main... --name-status --diff-filter=d', (err, status) => {
+        if (err) {
+            console.error(`exec error while retrieving commit files list: ${err}`);
+            process.exit(-1);
+        }
+
+        const files = status.trim().split('\n').map(s => s.substring(s.lastIndexOf('\t') + 1));
+        if (files.filter(name => name === NOCHANGE_PREFIX + branch || /^\.changeset\/[-\w]+\.md$/.test(name)).length === 0) {
+            process.exit(-1);
+        }
+    });
+}); 
